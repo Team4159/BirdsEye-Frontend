8c18dcc928be5ab9950460d09bd71720487c0c1f
Emmabot <maliu2@s.sfusd.edu>
2023-01-27 20:50:27 -0800

    add navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'matchscout.dart';
+
 void main() => runApp(const BirdsEye());
 
 class BirdsEye extends StatelessWidget {
@@ -9,26 +11,46 @@ class BirdsEye extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Bird's Eye",
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.blue,
+      title: "Bird's Eye",
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Bird's Eye"),
         ),
-        home: Scaffold(
-            body: GridView.count(
-          crossAxisCount: 2,
+        drawer: Drawer(
+            child: ListView(
           children: [
-            ListTile(title: Text("Match Scouting")),
-            ListTile(title: Text("Pit Scouting")),
-            ListTile(title: Text("Analysis & Ranking"))
+            ListTile(
+              title: const Text("Match Scouting"),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const MatchScout()));
+              },
+            ),
           ],
         )));
   }
--- lib/matchscout.dart
+++ lib/matchscout.dart
@@ -1,1 +1,11 @@
 // get latest match scouting form -> cache -> ensure app version matches -> process into a form -> send to server
+import 'package:flutter/cupertino.dart';
+
+class MatchScout extends StatelessWidget {
+  const MatchScout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
