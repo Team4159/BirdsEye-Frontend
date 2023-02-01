8f238920d78e718fdc0a332ae0cf255e64ad23cf
Calvin Lu <calvinlu@live.com>
2023-01-31 18:24:30 -0800

    Add basic structure for match scouting page

--- lib/matchscout.dart
+++ lib/matchscout.dart
@@ -1,1 +1,35 @@
 // get latest match scouting form -> cache -> ensure app version matches -> process into a form -> send to server
+import 'package:flutter/material.dart';
+
+class MatchScout extends StatelessWidget {
+  const MatchScout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+class Question extends StatelessWidget {
+  final String question;
+  final AnswerType answerType;
+
+  const Question({
+    required super.key,
+    required this.question,
+    required this.answerType,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(),
+    );
+  }
+}
+
+enum AnswerType {
+  number,
+  string,
+  date,
+}
