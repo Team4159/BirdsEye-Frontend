9f109c7f9773ac852623dda605cf42638b604bd9
Maxwell Liu <maliu2@s.sfusd.edu>
2023-02-04 01:53:21 -0800

    upload broken portions for future work

--- lib/pitscout.dart
+++ lib/pitscout.dart
@@ -1,9 +1,30 @@
-// fetch pit scouting questions -> cache on device -> text field entries -> send back to server
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 import 'main.dart';
 import 'matchscout.dart';
-import 'web.dart';
+
+enum PitScoutQuestionTypes {
+  text
+}
+
+Future<List<FormField?>> getPitScoutQuestions() async {
+  // await http.get(Uri.parse("https://api.lol.xd/pitscout"))).body
+  return jsonDecode('''{
+    "How Robot?": "text",
+    "Literally Trolled": "text",
+    "Break": "lmao"
+  }''', reviver: (k, v) {
+    if (k is String && v is String && PitScoutQuestionTypes.values.any((t) => t.name == v)) {
+      switch (PitScoutQuestionTypes.values.byName(v)) {
+        case PitScoutQuestionTypes.text:
+          return TextFormField(decoration: InputDecoration(labelText: k, border: const OutlineInputBorder()));
+      }
+    }
+    return null;
+  });
+}
 
 class PitScout extends StatelessWidget {
   const PitScout({super.key});
@@ -22,18 +43,14 @@ class PitScout extends StatelessWidget {
               title: const Text("Match Scouting"),
               onTap: () {
                 Navigator.pop(context);
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const MatchScout()));
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const MatchScout()));
               },
             ),
             ListTile(
               title: const Text("Pit Scouting"),
               onTap: () {
                 Navigator.pop(context);
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => const PitScout()));
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const PitScout()));
               },
             ),
             const AboutListTile(
@@ -42,22 +59,6 @@ class PitScout extends StatelessWidget {
             )
           ],
         )),
-        body: Container(
-            padding: const EdgeInsets.all(20),
-            child: FutureBuilder(
-                initialData: const <Map<String, String>>[],
-                future: getPitScoutQuestions(),
-                builder: (context, snapshot) => Form(
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    child: ListView.builder(
-                        itemCount: snapshot.data?.length ?? 0,
-                        itemBuilder: (context, index) {
-                          var li = snapshot.data?[index];
-                          if (li == null) return Container(color: Colors.red);
-                          return TextFormField(
-                              decoration: InputDecoration(
-                                  labelText: li.keys.first,
-                                  border: const OutlineInputBorder()));
-                        })))));
+        body: Container(padding: const EdgeInsets.all(20), child: Form(autovalidateMode: AutovalidateMode.onUserInteraction, child: FutureBuilder(initialData: const <FormField>[], future: getPitScoutQuestions(), builder: (context, snapshot) => ListView.builder(itemCount: snapshot.data?.length ?? 0, itemBuilder: (context, index) => snapshot.data?[index] ?? Container(color: Colors.red))))));
   }
 }
--- lib/web.dart
+++ lib/web.dart
@@ -1,19 +1,10 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-// TODO: Implement Caching System
+// TODO: Implement Caching System, send back to server
 
 Future<num> currentSeason() async {
-  var resp =
-      await http.get(Uri.parse("https://frc-api.firstinspires.org/v3.0/"));
+  var resp = await http.get(Uri.parse("https://frc-api.firstinspires.org/v3.0/"));
   return jsonDecode(resp.body).currentSeason;
-}
-
-Future<List<Map<String, String>>> getPitScoutQuestions() async {
-  // return jsonDecode(
-  //     (await http.get(Uri.parse("https://api.lol.xd/pitscout"))).body);
-  return [
-    {"How Robot?": "text"},
-    {"Literally Trolled": "text"}
-  ];
 }
